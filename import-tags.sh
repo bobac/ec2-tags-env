ec3e84d26c90538f7b9b285ee09b10116b849948
Robert Houser <bobac@bobac.net>
2020-03-06 14:11:34 +0100

    changed to aws ubuntu, added AWS_REGION export

--- import-tags.sh
+++ import-tags.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+aws_region=$(/usr/bin/curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -c -r .region)
 
 get_instance_tags () {
     instance_id=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/instance-id)
-    echo $(/usr/local/bin/aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id")
+    echo $(/usr/bin/aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" --region $aws_region)
 }
 
 get_ami_tags () {
     ami_id=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/ami-id)
-    echo $(/usr/local/bin/aws ec2 describe-tags --filters "Name=resource-id,Values=$ami_id")
+    echo $(/usr/bin/aws ec2 describe-tags --filters "Name=resource-id,Values=$ami_id" --region $aws_region)
 }
 
 tags_to_env () {
@@ -16,6 +17,7 @@ tags_to_env () {
     for key in $(echo $tags | /usr/bin/jq -r ".[][].Key"); do
         value=$(echo $tags | /usr/bin/jq -r ".[][] | select(.Key==\"$key\") | .Value")
         key=$(echo $key | /usr/bin/tr '-' '_' | /usr/bin/tr '[:lower:]' '[:upper:]')
+        echo $key=$value
         export $key="$value"
     done
 }
@@ -25,3 +27,4 @@ instance_tags=$(get_instance_tags)
 
 tags_to_env "$ami_tags"
 tags_to_env "$instance_tags"
+export AWS_REGION="$aws_region"
